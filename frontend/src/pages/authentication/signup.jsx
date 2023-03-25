a2d60ee1dad9e317d5973c7fc6dc0a9e9c1ac1a0
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2023-03-25 16:17:44 +0530

    Dhiraj/BE_index.js_some changes

--- frontend/src/pages/authentication/signup.jsx
+++ frontend/src/pages/authentication/signup.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function SingUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   return (
-    <div>SingUp</div>
+    <div>
+      <form>
+        <h1> Logo </h1>
+        <h3> Create a accoutn</h3>
+        <div className='mail'>
+          <label htmlFor="email"> Email: </label>
+          <input type="email" id='email' value={email}/>
+        </div>
+        <div className='create-password'>
+
+        </div>
+      </form>
+    </div>
   )
 }
