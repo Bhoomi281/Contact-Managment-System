8b5cf7254f95157eac62bd0722052e78b6d7481c
naziya <naaziyapatel36@gmail.com>
2023-03-27 13:51:24 +0530

    naziya/ BE added

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -2,6 +2,74 @@ const router = require("express").Router()
 
 const {Contacts} = require("../models/contacts.js");
 //posting 
+const Contacts = require("../models/contacts.js");
+//posting 
+const multer = require("multer");
+const csv = require("csvtojson");
+const fs=require('fs');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads');
+
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`);
+    }
+  });
+const upload = multer({ storage: storage });
+
+// Post contacts End Point
+router.post("/contacts", upload.single("file"),async  (req, res) => {
+    console.log(req.file)
+    
+    if (!req.file) {
+      return res.status(400).send("No file was uploaded.");
+    }
+    const file = req.file;
+    const fileContents = fs.readFileSync(file.path, 'utf-8');
+  
+    await csv()
+      .fromString(fileContents)
+      .then(async (jsonObj) => {
+        
+        jsonObj.forEach(obj => {
+          obj.user = req.user.data; // Get user id from frontend to add to each object
+        });
+        
+        // Json insertion to database
+
+        let files=await Contacts.insertMany(jsonObj);
+        
+        console.log(files);
+
+        fs.unlink(file.path, (err) => {
+          if (err) throw err;
+          console.log(`${file.path} was deleted`);
+        });
+  
+        res.send("File uploaded and parsed");
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error parsing CSV file");
+      });
+  });
+
+
+// Delete contact api 
+
+router.delete('/delete',async (req,res)=>{
+    try{
+    let delIds=req.headers['ids'].split(',');
+    console.log(delIds);
+    let deleted= await Contacts.deleteMany({ _id: { $in: delIds } });
+    console.log(deleted.deletedCount);
+    res.status(200).send({status: "Success"})
+    }catch(e){
+        res.status(500).send({status: "Failed", error : e})
+    }
+})
 
 router.post('/contacts',async(req,res)=>{
     try{
