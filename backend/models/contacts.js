853eaded4e11020faf0ef821f020c0fc3d79a8c2
naziya <naaziyapatel36@gmail.com>
2023-03-27 13:50:22 +0530

    naziya/delete api added

--- backend/models/contacts.js
+++ backend/models/contacts.js
@@ -6,14 +6,15 @@ const ObjectId = Schema.ObjectId;
 
 //create schema
 const ContactsSchema = new Schema({
-    email: {type:String,required:true,unique:true},
-    name: {type:String, required:true},
-    designation: {type:String, required:true},
-    company: {type:String, required:true},
-    industry: {type:String, required:true},
-    phone: {type:Number, required:true},
-    country: {type:String, required:true},
-    useRef:{type: String}
+    name: String,
+    designation: String,
+    company: String,
+    industry: String,
+    email: String,
+    phone: String,
+    country: String,
+    user : {type : mongoose.Schema.Types.ObjectId , ref: 'User' }
+
 })
 
 //create collection/model using this schema
